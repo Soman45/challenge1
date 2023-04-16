a053ae6c85ad75bda7cf2f33a79943c2d55b6ef9
Somandaru Aji Cakrayudha <s.a.cakrayudha@gmail.com>
2023-04-16 14:59:10 +0700

    mindah semua file html ke ejs dan static file menggunakan middleware

--- index.js
+++ index.js
@@ -2,13 +2,44 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.get('/Home', (req, res)=>{
-    res.sendFile(__dirname+'/index.html')
+
+//VIEW ENGINE
+app.set('view engine','ejs');
+
+//MIDDLEWARE
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+
+//ROUTE HALAMAN
+app.get('/home', (req, res)=>{
+    res.render(__dirname+'/views/index')
+});
+
+app.get('/game', (req, res)=>{
+    res.render(__dirname+'/views/game')
+});
+
+app.get('/register', (req, res)=>{
+    res.render(__dirname+'/views/register')
+});
+
+
+//REGISTER
+app.use(express.urlencoded({extended:false}));
+
+app.get('/register', function(req,res){
+    res.sendFile(__dirname+'/register.html');
+});
+
+app.post('/hasil',function(req,res){
+    console.log('isi', req.body);
+    res.send(`nama : ${req.body.nama}, Email : ${req.body.email}, Email : ${req.body.password}`);
 });
 
 
 
-
-app.listen(3000,()=>{
-    console.log('Server berjalan di PORT 3000');
+const port = 3000
+app.listen(port,()=>{
+    console.log(`Server berjalan di PORT ${port}`);
 });
